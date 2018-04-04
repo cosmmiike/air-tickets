c47ac2cde2fb355b112a981b017e3348d271a007
Miike Milller <askwis@icloud.com>
2018-04-04 08:40:53 +0200

    Trying 3

--- app/views/route_flight_view/index.php
+++ app/views/route_flight_view/index.php
@@ -2,7 +2,7 @@
   <head>
     <title>Routes on HTML format</title>
     <meta charset="utf-8"/>
-    <link href="../../aviatickets/public/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="../public/css/style.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
     <h1>Подбор авиарейсов</h1>
@@ -23,7 +23,7 @@
     <div class="copyright">
       <p>Михаил Шаймарданов, МГТУ им. Н.Э. Баумана, 2016 г.</p>
     </div>
-    <script type="text/javascript" src="../../aviatickets/public/js/jquery-3.1.1.js"></script>
-    <script type="text/javascript" src="../../aviatickets/public/js/main.js"></script>
+    <script type="text/javascript" src="../public/js/jquery-3.1.1.js"></script>
+    <script type="text/javascript" src="../public/js/main.js"></script>
   </body>
 </html>
